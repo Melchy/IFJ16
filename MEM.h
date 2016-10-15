d8a8bf12990d2c39d2b4b49a0b3e288db4b3c96b
Michal Motyčka <mychalmotycka@gmail.com>
2016-10-15 21:13:48 +0200

    Mem uprava hlavicky

--- MEM.h
+++ MEM.h
@@ -7,21 +7,10 @@
 
 #define MEM_free(p) MEM_ffree(p); p = NULL
 
-typedef struct t_Address t_Address;
-
-struct t_Address{
+typedef struct t_Address{
 	void *ptr;
-	t_Address *next;
-};
-
-/* Prida item do hashovaci tabulky, nealokuje zadnou pamet */
-void hash_add(t_Address *address);
-
-/* Odstrani item z hashovaci tabulky, neuvolnuje pamet */
-t_Address *hash_remove(void *ptr);
-
-/* Hashovaci funkce */
-int hashf_mem(long address);
+	struct t_Address *next;
+} t_Address;
 
 /* Alokuje uzitecnou pamet i pamet pro strukturu adresy a zavola hash_add */
 void *MEM_malloc(size_t size);
